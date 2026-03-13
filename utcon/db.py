2733a736b4faecd537e5fcd69643f8cdf0635bb5
Bridged1707 <shinkensei374@gmail.com>
2026-03-13 14:41:30 +0000

    push

--- utcon/db.py
+++ utcon/db.py
@@ -1,5 +1,8 @@
 import asyncpg
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 _pool = None
 
@@ -8,12 +11,21 @@ async def connect():
 
     global _pool
 
+    host = os.getenv("UTDB_HOST")
+    port = os.getenv("UTDB_PORT")
+    user = os.getenv("UTDB_USER")
+    password = os.getenv("UTDB_PASSWORD")
+    database = os.getenv("UTDB_NAME")
+
+    if not port:
+        raise RuntimeError("UTDB_PORT not set in .env")
+
     _pool = await asyncpg.create_pool(
-        host=os.getenv("UTDB_HOST"),
-        port=int(os.getenv("UTDB_PORT")),
-        user=os.getenv("UTDB_USER"),
-        password=os.getenv("UTDB_PASSWORD"),
-        database=os.getenv("UTDB_NAME"),
+        host=host,
+        port=int(port),
+        user=user,
+        password=password,
+        database=database,
         min_size=2,
         max_size=10,
     )
