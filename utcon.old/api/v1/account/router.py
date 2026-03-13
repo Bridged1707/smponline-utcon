c9c78f3c1f7fd103b27a2a5d9bbe79ebc98f91f0
Bridged1707 <shinkensei374@gmail.com>
2026-03-13 14:38:28 +0000

    push

--- utcon.old/api/v1/account/router.py
+++
@@ -1,3 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/v1/account", tags=["account"])
--- utcon.old/schemas/account.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class RegisterAccount(BaseModel):
-    discord_uuid: str
-    mc_uuid: str
-    mc_name: str
