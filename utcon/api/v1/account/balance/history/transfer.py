6b0199ae94e909ba93f969cb9db8d9b9aefa988d
Bridged1707 <shinkensei374@gmail.com>
2026-03-13 14:46:51 +0000

    push

--- utcon/api/v1/account/balance/history/transfer.py
+++ utcon/api/v1/account/balance/history/transfer.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter
-
 from utcon import db
 
 router = APIRouter(prefix="/v1/account/balance/history", tags=["balance"])
@@ -13,15 +12,13 @@ async def transfer_history(discord_uuid: str, limit: int = 50):
         rows = await conn.fetch(
             """
             SELECT *
-            FROM transactions
-            WHERE
-                from_discord_uuid=$1
-                OR to_discord_uuid=$1
+            FROM balance_transfers
+            WHERE discord_uuid=$1
             ORDER BY created_at DESC
             LIMIT $2
             """,
             discord_uuid,
-            limit,
+            limit
         )
 
     return [dict(r) for r in rows]
